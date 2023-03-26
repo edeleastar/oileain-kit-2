52f56a8a88c7537e522bc24e82fd2c717b466521
Eamonn de Leastar <edeleastar@wit.ie>
2023-03-26 08:15:58 +0100

    basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Header from "../lib/Header.svelte";
+</script>
+
+<Header />
+<div class="container">
+	<div class="columns">
+		<div class="column is-four-fifths">
+			<slot />
+		</div>
+	</div>
+</div>
