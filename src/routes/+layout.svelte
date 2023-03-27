626587fa7f6e1210dc26f5e67531b8137944b082
Eamonn de Leastar <edeleastar@wit.ie>
2023-03-27 13:45:36 +0100

    include sidebar on the layout for all pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
+	import SideBar from "$lib/SideBar.svelte";
 	import Header from "../lib/Header.svelte";
+	import { onMount } from "svelte";
+	import { oileainService } from "../services/oileain-service";
+	import type { IslandGroup } from "../services/oileain-types";
+
+	let allCoasts: IslandGroup[] = [];
+	onMount(async () => {
+		allCoasts = await oileainService.getCoasts();
+	});
 </script>
 
 <Header />
 <div class="container">
 	<div class="columns">
-		<div class="column">
+		<div class="column is-four-fifths">
 			<slot />
+		</div>
+		<div class="column">
+			<SideBar {allCoasts} />
 		</div>
 	</div>
 </div>
