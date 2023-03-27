35f424df3d04d0cf980ecd336c7058284dca1af2
Eamonn de Leastar <edeleastar@wit.ie>
2023-03-27 09:49:59 +0100

    render marker for each island

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import LeafletMap from "$lib/LeafletMap.svelte";
+	import type { PageData } from "./$types";
+	export let data: PageData;
 </script>
 
-<LeafletMap height={85} />
+<LeafletMap height={85} markerLayers={data.markerLayers} />
