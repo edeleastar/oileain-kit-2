8a98abc4098a61740a42bdc0caf25f8061bf0a1a
Eamonn de Leastar <edeleastar@wit.ie>
2024-09-04 18:18:18 +0100

    fix poi view

--- src/routes/poi/[id]/+page.svelte
+++ src/routes/poi/[id]/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+  import type { PageData } from "./$types";
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  export let data: PageData;
   import IslandCoordinates from "$lib/islands/IslandCoordinates.svelte";
   import IslandDescription from "$lib/islands/IslandDescription.svelte";
   import IslandMap from "$lib/maps/IslandMap.svelte";
+  import { sharedIsland, sharedMarker } from "$lib/model/shared-state";
+
+  page.subscribe((path) => {
+    sharedMarker.value = data.marker;
+    sharedIsland.value = data.island;
+  });
+
+  onMount(async () => {
+    sharedMarker.value = data.marker;
+    sharedIsland.value = data.island;
+  });
 </script>
 
 <div class="columns">
